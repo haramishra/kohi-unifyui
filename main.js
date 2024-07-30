238b299e581914d18e6975b14a2da50f8a95e1fc
hara <haraprasad45@gmail.com>
2024-07-30 19:39:02 +0530

    cross origin issue fixed

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import "./style.css"
+import "./style.css";
+
+console.log("Hello from main.js");
+
+window.onload = () => {
+  const bodyContent = document.body.innerHTML;
+  window.parent.postMessage(
+    { type: "iframeContent", content: bodyContent },
+    "*"
+  );
+};
